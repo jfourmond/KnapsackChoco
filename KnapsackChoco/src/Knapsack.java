d9f52f7f6b8e568865544f34a9026740f9ed0846
Stan <Stan@stans-mbp>
2016-11-26 07:29:13 +0100

    Ajout de KnapsackSolverExistingConstraint qui utilise la contrainte existante knapsack()

--- KnapsackChoco/src/Knapsack.java
+++ KnapsackChoco/src/Knapsack.java
@@ -3,11 +3,14 @@ import java.io.IOException;
 import data.DataReader;
 import data.DataReaderException;
 import solver.KnapsackSolver;
+import solver.KnapsackSolverExistingConstraint;
 
 public class Knapsack {
 
 	public static void main(String[] args) {
+
 		DataReader data = null;
+
 		try {
 			data = new DataReader("data/p01/p01_w.txt", "data/p01/p01_p.txt", "data/p01/p01_c.txt");
 		} catch (IOException e) {
@@ -15,7 +18,15 @@ public class Knapsack {
 		} catch (DataReaderException e) {
 			e.printStackTrace();
 		}
-		KnapsackSolver solver = new KnapsackSolver(data.getNbItems(), data.getCapacity(), data.getWeightsTab(), data.getProfitsTab());
+
+		System.out.println("\nWith KnapsackSolver");
+		KnapsackSolver solver = new KnapsackSolver(data.getNbItems(), data.getCapacity(), data.getWeightsTab(),
+				data.getProfitsTab());
+		solver.solve();
+
+		System.out.println("\nWith KnapsackSolverExistingConstraint");
+		KnapsackSolverExistingConstraint solver2 = new KnapsackSolverExistingConstraint(data.getNbItems(),
+				data.getCapacity(), data.getWeightsTab(), data.getProfitsTab());
 		solver.solve();
 	}
 
---
+++ KnapsackChoco/src/solver/KnapsackSolverExistingConstraint.java
@@ -0,0 +1,53 @@
+package solver;
+
+import org.chocosolver.solver.Model;
+import org.chocosolver.solver.Solver;
+import org.chocosolver.solver.variables.IntVar;
+
+public class KnapsackSolverExistingConstraint {
+
+	private int capacity;
+	private int[] weights;
+	private int[] profits;
+
+	private int n;
+
+	Model model;
+
+	public KnapsackSolverExistingConstraint(int _nbItems, int _capacity, int[] _weights, int[] _profits) {
+
+		this.capacity = _capacity;
+		this.weights = _weights;
+		this.profits = _profits;
+		this.n = _nbItems;
+	}
+
+	public void solve() {
+
+		// 1. Modelling part
+		model = new Model("KNAPSACK PROBLEM WITH EXISTING CONSTRAINT");
+
+		IntVar[] occurences = new IntVar[this.n];
+		for (int i = 0; i < this.n; i++) {
+			occurences[i] = model.intVar(0, 1);
+		}
+
+		IntVar weightSum = model.intVar(0, this.capacity, true);
+		IntVar energySum = model.intVar(0, 920);
+
+		model.knapsack(occurences, weightSum, energySum, this.weights, this.profits).post();
+
+		// 2. Solving part
+		Solver solver = model.getSolver();
+
+		if (solver.solve()) {
+
+			System.out.println("weightSum : " + weightSum.getValue());
+
+			for (int i = 0; i < this.n; i++) {
+				System.out.println(occurences[i].getValue());
+			}
+		}
+	}
+
+}
