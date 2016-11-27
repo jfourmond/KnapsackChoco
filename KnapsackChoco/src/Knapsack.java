c8b8c59a64705b725299c886900b5952c3425129
Stan <Stan@stans-mbp>
2016-11-27 08:58:06 +0100

    Ajout classe de test

--- KnapsackChoco/src/Knapsack.java
+++ KnapsackChoco/src/Knapsack.java
@@ -10,28 +10,55 @@ public class Knapsack {
 		DataReader data = null;
 		try {
 			// p01
-			// data = new DataReader("data/p01/p01_w.txt", "data/p01/p01_p.txt", "data/p01/p01_c.txt");
+			// Max profit : 309
+			// Max poids : 165
+			// OK pour KnapsackSolverInputOrderUBSearch
+			// data = new DataReader("data/p01/p01_w.txt", "data/p01/p01_p.txt",
+			// "data/p01/p01_c.txt");
 
-			// p02 : Max profit : 26
-			// data = new DataReader("data/p02/p02_w.txt", "data/p02/p02_p.txt", "data/p02/p02_c.txt");
+			// p02
+			// Max profit : 51
+			// Max poids : 26
+			// data = new DataReader("data/p02/p02_w.txt", "data/p02/p02_p.txt",
+			// "data/p02/p02_c.txt");
 
 			// p03
-			// data = new DataReader("data/p03/p03_w.txt", "data/p03/p03_p.txt", "data/p03/p03_c.txt");
+			// Max profit : 150
+			// Max poids : 190
+			// OK avec KnapsackSolverExistingConstraint
+			// OK avec KnapsackSolverDefaultStrategy
+			// data = new DataReader("data/p03/p03_w.txt", "data/p03/p03_p.txt",
+			// "data/p03/p03_c.txt");
 
 			// p04
-			// data = new DataReader("data/p04/p04_w.txt", "data/p04/p04_p.txt", "data/p04/p04_c.txt");
+			// Max profit : 107
+			// Max poids : 50
+			// data = new DataReader("data/p04/p04_w.txt", "data/p04/p04_p.txt",
+			// "data/p04/p04_c.txt");
 
 			// p05
-			// data = new DataReader("data/p05/p05_w.txt", "data/p05/p05_p.txt", "data/p05/p05_c.txt");
-			
+			// Max profit : 900
+			// Max poids : 104
+			// data = new DataReader("data/p05/p05_w.txt", "data/p05/p05_p.txt",
+			// "data/p05/p05_c.txt");
+
 			// p06
-			// data = new DataReader("data/p06/p06_w.txt", "data/p06/p06_p.txt", "data/p06/p06_c.txt");
-			
+			// Max profit : 1735
+			// Max poids : 169
+			data = new DataReader("data/p06/p06_w.txt", "data/p06/p06_p.txt", "data/p06/p06_c.txt");
+
 			// p07
-			// data = new DataReader("data/p07/p07_w.txt", "data/p07/p07_p.txt", "data/p07/p07_c.txt");
-			
-			// p08 : Max profit : 13549094
-			data = new DataReader("data/p08/p08_w.txt", "data/p08/p08_p.txt", "data/p08/p08_c.txt");
+			// Max profit : 1458
+			// Max poids : 749
+			// data = new DataReader("data/p07/p07_w.txt", "data/p07/p07_p.txt",
+			// "data/p07/p07_c.txt");
+
+			// p08
+			// Max profit : 13 549 094
+			// Max poids : 6 402 560
+			// data = new DataReader("data/p08/p08_w.txt", "data/p08/p08_p.txt",
+			// "data/p08/p08_c.txt");
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		} catch (DataReaderException e) {
@@ -39,20 +66,29 @@ public class Knapsack {
 		}
 
 		System.out.println("\nWith KnapsackSolverInputOrderUBSearch");
-		KnapsackSolver solver = new KnapsackSolverInputOrderUBSearch(data.getNbItems(), data.getCapacity(), data.getWeightsTab(), data.getProfitsTab());
+		KnapsackSolver solver = new KnapsackSolverInputOrderUBSearch(data.getNbItems(), data.getCapacity(),
+				data.getWeightsTab(), data.getProfitsTab());
 		solver.solve();
 
 		System.out.println("\nWith KnapsackSolverDefaultStrategy");
-		KnapsackSolver solver2 = new KnapsackSolverDefaultStrategy(data.getNbItems(), data.getCapacity(), data.getWeightsTab(), data.getProfitsTab());
+		KnapsackSolver solver2 = new KnapsackSolverDefaultStrategy(data.getNbItems(), data.getCapacity(),
+				data.getWeightsTab(), data.getProfitsTab());
 		solver2.solve();
 
 		System.out.println("\nWith KnapsackSolverExistingConstraint");
-		KnapsackSolver solver3 = new KnapsackSolverExistingConstraint(data.getNbItems(), data.getCapacity(), data.getWeightsTab(), data.getProfitsTab());
+		KnapsackSolver solver3 = new KnapsackSolverExistingConstraint(data.getNbItems(), data.getCapacity(),
+				data.getWeightsTab(), data.getProfitsTab());
 		solver3.solve();
-		
+
 		System.out.println("\nWith KnapsackSolverActivityBasedSearch");
-		KnapsackSolver solver4 = new KnapsackSolverActivityBasedSearch(data.getNbItems(), data.getCapacity(), data.getWeightsTab(), data.getProfitsTab());
+		KnapsackSolver solver4 = new KnapsackSolverActivityBasedSearch(data.getNbItems(), data.getCapacity(),
+				data.getWeightsTab(), data.getProfitsTab());
 		solver4.solve();
+
+		System.out.println("\nWith KnapsackSolverMinDomLBSearch");
+		KnapsackSolver solver5 = new KnapsackSolverMinDomLBSearch(data.getNbItems(), data.getCapacity(),
+				data.getWeightsTab(), data.getProfitsTab());
+		solver5.solve();
 	}
 
 }
